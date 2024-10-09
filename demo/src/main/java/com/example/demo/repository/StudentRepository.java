515666dc1471dda99494480e973f39f6aabf0ab2
Killowa <serikakhmetovadil@mail.ru>
2024-10-09 22:22:15 +0500

    Implement Update functionality for Student entity

--- demo/src/main/java/com/example/demo/repository/StudentRepository.java
+++ demo/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,5 +1,5 @@
 package com.example.demo.repository;
-
+import java.util.UUID;
 import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
