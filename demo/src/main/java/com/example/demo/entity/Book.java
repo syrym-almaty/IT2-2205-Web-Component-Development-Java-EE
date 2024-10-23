47b7991a9abeda390e7a4a891d2d8599a0a31fe0
maratovarsen <arsen.maratov.99@mail.ru>
2024-10-24 00:41:39 +0500

    upd:Arsen lab5-1

---
+++ demo/src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,78 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;  // Убедитесь, что используете @Id из JPA
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    private String title;
+
+    private String author;
+
+    private String isbn;
+
+    private boolean available;
+
+    // Конструктор по умолчанию
+    public Book() {
+    }
+
+    // Конструктор с параметрами
+    public Book(String title, String author, String isbn, boolean available) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+    }
+
+    // Геттеры и сеттеры
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
