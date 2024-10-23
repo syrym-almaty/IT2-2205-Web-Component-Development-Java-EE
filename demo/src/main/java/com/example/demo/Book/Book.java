51d274bbe79b2260e170269122ae66b0819ae5d8
ermaq <nursultanermagambet@gmail.com>
2024-10-23 16:56:07 +0500

    lab 5 done

---
+++ demo/src/main/java/com/example/demo/Book/Book.java
@@ -0,0 +1,42 @@
+package com.example.demo.Book;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(columnDefinition = "uuid")
+    private UUID id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+
+    // Constructors
+    public Book() {
+    }
+
+    public Book(String title, String author, String isbn, boolean available) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+    }
+}
