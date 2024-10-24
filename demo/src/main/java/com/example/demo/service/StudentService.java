c6c32b93de019c61eaa4d13a0b0e01438064b631
022Ali02 <sayrashev04@gmail.com>
2024-10-24 12:43:07 +0500

    upd:Sairashev Alikhan lab-5-1

--- demo/src/main/java/com/example/demo/service/StudentService.java
+++ demo/src/main/java/com/example/demo/service/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.Exception.ResourceNotFoundException;
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
